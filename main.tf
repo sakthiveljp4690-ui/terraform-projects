37fb4d8cc5c6e67c461d4fe47952463acb0391f5
“sakthivel.venkatesan” <sakthiveljp4960@gmail.com>
2026-02-23 18:46:01 +0900

    pub-private-architecture

--- main.tf
+++ main.tf
@@ -1,16 +1,24 @@
 data "aws_ami" "amazon_linux" {
     most_recent = true
     owners = ["amazon"]
-
+    
     filter {
         name = "name"
         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
     }
 }
 
+data "aws_security_group" "nat_sg" {
+    id = aws_security_group.public_security_groups.id
+}
+
+data "aws_security_group" "private_sg" {
+    id = aws_security_group.private_security_groups.id
+}
+
+
 resource "aws_iam_role" "ec2_ssm_role" {
     name = "ec2_ssm_role"
-
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
@@ -34,14 +42,14 @@ resource "aws_iam_instance_profile" "ec2_profile" {
     name = "ec2_instance_profile"
     role = aws_iam_role.ec2_ssm_role.name 
 }
-resource "aws_security_group" "security_groups" {
+resource "aws_security_group" "public_security_groups" {
+    vpc_id = aws_vpc.free_tier_vpc.id
 
     ingress {
-        description = "webserver"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["10.1.2.0/24"]
     }
 
     egress {
@@ -51,26 +59,116 @@ resource "aws_security_group" "security_groups" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
-        Name = "allow tcp sg"
+        Name = "allow public traffic"
     }
 }
 
-resource "aws_eip" "free_tier_eip" {
-    instance = aws_instance.free_tier_ec2.id
-    domain = "vpc"
+resource "aws_security_group" "private_security_groups" {
+    vpc_id = aws_vpc.free_tier_vpc.id
+    
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = [data.aws_security_group.nat_sg.id]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = [data.aws_security_group.nat_sg.id]
+    }
+    tags = {
+        Name = "allow private traffic"
+    }
 }
 
-resource "aws_instance" "free_tier_ec2" {
+resource "aws_instance" "free_tier_nat_instance" {
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.public_security_groups.id ]
+    subnet_id = aws_subnet.public_subnet.id
+    associate_public_ip_address = true
+    source_dest_check = false
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo sysctl -w net.ipv4.ip_forward=1
+              sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+              EOF
+    tags = {
+        Name = "terraform-nat-instance"
+    }
+}
+
+resource "aws_instance" "free_tier_private_instance" {
     ami = data.aws_ami.amazon_linux.id
     instance_type = "t3.micro"
     iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-    vpc_security_group_ids = [ aws_security_group.security_groups.id ]
-    root_block_device {
-    volume_size = 8
-    volume_type = "gp3"
-    }
+    vpc_security_group_ids = [ aws_security_group.private_security_groups.id ]
+    subnet_id = aws_subnet.private_subnet.id
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install nginx -y
+              sudo amazon-linux-extras install nginx1
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              sudo echo "<h1>Terraform Deployed This Server</h1>" > /usr/share/nginx/html/index.html
+              EOF
     tags = {
         Name = "terraform-free-tier"
+    }
+}
+
+resource "aws_vpc" "free_tier_vpc" {
+    enable_dns_hostnames = true
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "terraform-free-tier-vpc"
+    }
+}
+
+resource "aws_route_table" "free_tier_routetable" {
+  vpc_id = aws_vpc.free_tier_vpc.id
+}
+
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.free_tier_routetable.id
+    gateway_id = aws_internet_gateway.free_tier_igw.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_route" {
+    route_table_id = aws_route_table.free_tier_routetable.id
+    gateway_id = aws_instance.free_tier_nat_instance.id
+    destination_cidr_block = aws_subnet.private_subnet.cidr_block
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.free_tier_vpc.id
+    cidr_block = "10.1.1.0/24"
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.free_tier_vpc.id
+    cidr_block = "10.1.2.0/24"
+}
+
+resource "aws_route_table_association" "public_association" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.free_tier_routetable.id
+}
+
+resource "aws_route_table_association" "private_association" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.free_tier_routetable.id
+}
+
+resource "aws_internet_gateway" "free_tier_igw" {
+    vpc_id = aws_vpc.free_tier_vpc.id
+    tags = {
+      Name = "main"
     }
 }
 
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
-    value = aws_instance.free_tier_ec2.public_ip
+    value = aws_instance.free_tier_nat_instance.public_ip
+}
+
+output "private_ip" {
+    value = aws_instance.free_tier_private_instance.public_ip
 }
